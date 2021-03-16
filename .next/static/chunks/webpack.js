d5c95098fe957c62b804f1ff4cc293a27c8c35c2
Henry Fradley <fradleyhenry@gmail.com>
2021-03-15 21:05:37 -1000

    added favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e7d81459c94d382a7399";
+/******/ 	var hotCurrentHash = "49b8fa2c42b943c5a299";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Main from '../components/Main.jsx';
 import Skills from '../components/Skills.jsx';
 import Projects from '../components/Projects.jsx';
@@ -18,6 +19,12 @@ const Home = () => {
 
   return (
     <div>
+      <Head>
+        <title>Welcome!</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" type="image/png" href="./Favicon16x16.png" sizes="16x16"></link>
+        <link rel="icon" type="image/png" href="./Favicon32x32.png" sizes="32x32"></link>
+      </Head>
       <Main />
       <Skills />
       <Projects data={projectData} />
