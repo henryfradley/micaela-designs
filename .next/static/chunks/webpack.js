c75f77de0057e4c5248b56612cb4b55c04087409
Henry Fradley <fradleyhenry@gmail.com>
2021-03-15 20:06:03 -1000

    fixed sizing issue main page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "14b54174b3887b5089e3";
+/******/ 	var hotCurrentHash = "db94e02f8e11aa935468";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
