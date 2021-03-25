4afd3487320e657d2894e8e165ce1889d6ff3c84
Henry Fradley <fradleyhenry@gmail.com>
2021-03-24 19:13:45 -1000

    added italic to fix mobile issue

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "eae0639f961368f23a75";
+/******/ 	var hotCurrentHash = "b6e24b52eb3a4fdc0462";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
