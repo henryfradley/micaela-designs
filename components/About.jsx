78267250c8a69451179a5323e3f6c43b8582848a
Henry Fradley <fradleyhenry@gmail.com>
2022-12-09 18:47:07 -0800

    start

--- components/About.jsx
+++ components/About.jsx
@@ -1,15 +1,13 @@
-import Image from 'next/image';
-import Fade from 'react-reveal/Fade';
-
+import Image from "next/image";
+import Fade from "react-reveal/Fade";
 
 const myLoader = ({ src, width, quality }) => {
-  return `https://d2k5lcv0ndozn2.cloudfront.net/${src}?w=${width}&q=${quality || 75}`
-}
-
+  return `https://d2k5lcv0ndozn2.cloudfront.net/${src}?w=${width}&q=${
+    quality || 75
+  }`;
+};
 
 const About = () => {
-
-
   return (
     <div class="about" id="about">
       <div class="profileImage">
@@ -19,38 +17,45 @@ const About = () => {
           layout="fill"
           objectFit="cover"
         />
-
       </div>
       <div class="aboutText">
-      <Fade distance="5vh" delay={500} bottom>
-        <h1>ABOUT ME</h1>
-      </Fade>
-      <Fade distance="5vh" delay={600} bottom>
-        <p>Hi, thanks for stopping by! My name is Micaela and I’m a graphic designer from Mill Valley, CA. Having grown up near a big metropolitan city, I was constantly surrounded by art from a variety of cultures. My love for art inspired my profession as a graphic designer. I graduated from UH Manoa with a BFA in Graphic Design in the spring of 2019.</p>
-      </Fade>
-      <Fade distance="5vh" delay={650} bottom>
-        <p>Since then, I have been on the move expanding my portfolio, acquiring freelance projects, and working for the digital media agency, Ellemsee Media, in Downtown Honolulu.</p>
-      </Fade>
-      <Fade distance="5vh" delay={700} bottom>
-        <p>Have a project in mind you want to discuss? Let’s do it! Send me a message below to get started.</p>
-      </Fade>
+        <Fade distance="5vh" delay={500} bottom>
+          <h1>ABOUT ME</h1>
+        </Fade>
+        <Fade distance="5vh" delay={600} bottom>
+          <p>
+            Hi, thanks for stopping by! My name is Micaela and I’m a graphic
+            designer from Mill Valley, CA. Having grown up near a big
+            metropolitan city, I was constantly surrounded by art from a variety
+            of cultures. My love for art inspired my profession as a graphic
+            designer. I graduated from UH Manoa with a BFA in Graphic Design in
+            the spring of 2019.
+          </p>
+        </Fade>
+        <Fade distance="5vh" delay={650} bottom>
+          <p>
+            Since then, I have been on the move expanding my portfolio,
+            acquiring freelance projects, and working for the digital media
+            agency, Ellemsee Media, in Downtown Honolulu.
+          </p>
+        </Fade>
+        <Fade distance="5vh" delay={700} bottom>
+          <p>
+            Have a project in mind you want to discuss? Let’s do it! Send me a
+            message below to get started.
+          </p>
+        </Fade>
 
-      <Fade distance="5vh" delay={1100} bottom>
-        <div class="favorites">
-          <div>FAVORITE SNACK: KETTLE CORN</div>
-          <div>FAVORITE BOOK: READY PLAYER ONE</div>
-          <div>FAVORITE PASTIME: LEARNING PIANO</div>
-        </div>
-      </Fade>
-
-
-
+        <Fade distance="5vh" delay={1100} bottom>
+          <div class="favorites">
+            <div>FAVORITE SNACK: KETTLE CORN</div>
+            <div>FAVORITE BOOK: READY PLAYER ONE</div>
+            <div>FAVORITE PASTIME: LEARNING PIANO</div>
+          </div>
+        </Fade>
       </div>
-
     </div>
   );
 };
-
-
 
 export default About;
---
+++ components/Icon.jsx
@@ -0,0 +1,227 @@
+import React from "react";
+
+export const Icon = ({ variant }) => {
+  if (variant === "icon-1") {
+    return (
+      <svg
+        version="1.1"
+        id="Layer_1"
+        xmlns="http://www.w3.org/2000/svg"
+        // xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        viewBox="0 0 335 300"
+        // style="enable-background:new 0 0 335 300;"
+        // xmlSpace="preserve"
+      >
+        <g>
+          <polyline
+            className="st0"
+            points="211.51,255.2 211.51,275.12 33.49,275.12 33.49,24.88 211.51,24.88 211.51,82.39 	"
+          />
+          <line
+            className="st0"
+            x1="211.51"
+            y1="142.04"
+            x2="211.51"
+            y2="155.13"
+          />
+          <rect
+            x="172.36"
+            y="91.05"
+            className="st0"
+            width="78.28"
+            height="43.38"
+          />
+          <rect
+            x="120.64"
+            y="163.28"
+            className="st0"
+            width="180.87"
+            height="83.94"
+          />
+          <circle className="st0" cx="63.78" cy="56.75" r="16.78" />
+          <circle className="st0" cx="151.86" cy="206.08" r="16.78" />
+          <circle className="st0" cx="192.54" cy="113.37" r="8.79" />
+          <g>
+            <line
+              className="st0"
+              x1="95.01"
+              y1="51.25"
+              x2="128.09"
+              y2="51.25"
+            />
+            <line
+              className="st0"
+              x1="95.01"
+              y1="62.25"
+              x2="128.09"
+              y2="62.25"
+            />
+          </g>
+          <g>
+            <line
+              className="st0"
+              x1="213.84"
+              y1="107.87"
+              x2="236.67"
+              y2="107.87"
+            />
+            <line
+              className="st0"
+              x1="213.84"
+              y1="118.87"
+              x2="236.67"
+              y2="118.87"
+            />
+          </g>
+          <g>
+            <line
+              className="st0"
+              x1="236.67"
+              y1="200.58"
+              x2="280.48"
+              y2="200.58"
+            />
+            <line
+              className="st0"
+              x1="236.67"
+              y1="211.58"
+              x2="280.48"
+              y2="211.58"
+            />
+          </g>
+        </g>
+      </svg>
+    );
+  }
+  if (variant === "icon-2") {
+    return (
+      <svg
+        version="1.1"
+        id="Layer_1"
+        xmlns="http://www.w3.org/2000/svg"
+        // xmlns:xlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        viewBox="0 0 335 300"
+        style="enable-background:new 0 0 335 300;"
+        // xml:space="preserve"
+      >
+        <g>
+          <polygon
+            className="st0"
+            points="247.01,72.67 296.58,119.34 254.03,196.58 143.75,223.84 178.45,102.82 	"
+          />
+          <circle className="st0" cx="229.46" cy="136.07" r="14.25" />
+          <path
+            className="st0"
+            d="M38.42,49.14c0,0,97.43,0.83,35.51,90.83c-61.6,89.53-37.42,136.57,50.61,96.43c0.99-0.45,1.94-0.97,2.86-1.54
+		c2.6-1.62,8.52-5.39,16.36-11l76.4-75.6"
+          />
+        </g>
+      </svg>
+    );
+  }
+  if (variant === "icon-3") {
+    return (
+      <svg
+        version="1.1"
+        id="Layer_1"
+        xmlns="http://www.w3.org/2000/svg"
+        // xmlns:xlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        viewBox="0 0 335 300"
+        style="enable-background:new 0 0 335 300;"
+        // xml:space="preserve"
+      >
+        <g>
+          <rect
+            x="60.97"
+            y="50.59"
+            className="st0"
+            width="212.96"
+            height="198.81"
+          />
+          <rect
+            x="82.42"
+            y="100.48"
+            className="st0"
+            width="170.76"
+            height="66.92"
+          />
+          <g>
+            <line className="st0" x1="104.9" y1="188.35" x2="229.99" y2="188.35" />
+            <line className="st0" x1="104.9" y1="209.35" x2="229.99" y2="209.35" />
+            <line className="st0" x1="149.58" y1="227.22" x2="185.31" y2="227.22" />
+          </g>
+          <line className="st0" x1="60.81" y1="75.33" x2="274.19" y2="75.33" />
+          <circle className="st0" cx="202.88" cy="63.8" r="5.21" />
+          <circle className="st0" cx="227.01" cy="63.8" r="5.21" />
+          <circle className="st0" cx="251.14" cy="63.8" r="5.21" />
+        </g>
+      </svg>
+    );
+  }
+  if (variant === "icon-4") {
+    return (
+      <svg
+        version="1.1"
+        id="Layer_1"
+        xmlns="http://www.w3.org/2000/svg"
+        // xmlns:xlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        viewBox="0 0 335 300"
+        style="enable-background:new 0 0 335 300;"
+        // xml:space="preserve"
+      >
+        <g>
+          <path
+            className="st0"
+            d="M277.75,84.15c0-26.19-21.23-47.42-47.42-47.42s-47.42,21.23-47.42,47.42c0,19.13,11.33,35.59,27.64,43.09
+		v29.84c0,10.92,8.85,19.78,19.78,19.78s19.78-8.85,19.78-19.78v-29.84C266.42,119.74,277.75,103.27,277.75,84.15z"
+          />
+          <line className="st0" x1="210.73" y1="135.4" x2="249.82" y2="135.4" />
+          <line className="st0" x1="249.73" y1="147.94" x2="210.81" y2="147.94" />
+          <line className="st0" x1="249.29" y1="160.47" x2="211.25" y2="160.47" />
+          <line className="st0" x1="230.86" y1="99.82" x2="230.86" y2="135.16" />
+          <path
+            className="st0"
+            d="M240.32,85.18c0,5.08-9.2,14.2-9.2,14.2s-9.2-9.12-9.2-14.2s4.12-9.2,9.2-9.2
+		C236.2,75.98,240.32,80.1,240.32,85.18z"
+          />
+          <path className="st0" d="M202.03,82.8c0-14.91,12.08-26.99,26.99-26.99" />
+          <line className="st0" x1="230.78" y1="21.16" x2="230.78" y2="7.35" />
+          <line className="st0" x1="277.12" y1="40.14" x2="286.88" y2="30.38" />
+          <line className="st0" x1="185.02" y1="39.57" x2="175.26" y2="29.8" />
+          <line className="st0" x1="166.9" y1="83.89" x2="153.09" y2="83.89" />
+          <line className="st0" x1="294.95" y1="83.89" x2="309.91" y2="83.89" />
+          <polyline
+            className="st0"
+            points="230.42,190.08 230.42,291.32 167.23,291.32 	"
+          />
+          <rect
+            x="25.09"
+            y="129.52"
+            className="st0"
+            width="125.58"
+            height="163.13"
+          />
+          <line className="st0" x1="46.61" y1="151.43" x2="126.8" y2="151.43" />
+          <line className="st0" x1="46.61" y1="171.43" x2="126.8" y2="171.43" />
+          <line className="st0" x1="47.39" y1="210.89" x2="127.59" y2="210.89" />
+          <line className="st0" x1="47.39" y1="230.89" x2="127.59" y2="230.89" />
+          <g>
+            <line className="st0" x1="46.61" y1="192.43" x2="79.63" y2="192.43" />
+          </g>
+          <g>
+            <line className="st0" x1="93.61" y1="192.43" x2="126.63" y2="192.43" />
+          </g>
+        </g>
+      </svg>
+    );
+  }
+  return <></>
+};
